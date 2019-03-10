d14a84fded607df4399e565f2331029c9c6b85f9
Ryan Graciano <ryan@ryangraciano.com>
2019-03-09 16:10:46 -0800

    Changing how the choice() methods will be called

--- src/cards/ability.ts
+++ src/cards/ability.ts
@@ -47,123 +47,54 @@ export abstract class Ability {
         if (usesTargetingRules && cards)
             cards.filter(card => {
                 let eff = card.effective();
-                if (card.effective().untargetable || card.effective().invisible) return false;
+                if (eff.untargetable) return false;
+                else if (card.controller != card.game.activePlayer && eff.invisible) return false;
                 else return true;
             });
 
         phaseStack.addToStack(new Phase('ChooseAbilityTarget', ['AbilityChoice']));
     }
 
-    chooseTechLevelUnit(space: Card[], chooseNumber: number, choicesRequired: boolean, techLevel: TechLevel, usesTargetingRules: boolean) {
-        this.chooseUnit(
-            space,
-            chooseNumber,
-            choicesRequired,
-            usesTargetingRules,
-            techLevel,
-            techLevel,
-            card => card.techLevel == techLevel && card.cardType == 'Unit'
-        );
+    choicesUnits(space: Card[], leastTechLevel?: TechLevel, mostTechLevel?: TechLevel, extraFilter?: (card: Card) => boolean): Card[] {
+        return this.choicesCharacters(space, true, false, leastTechLevel, mostTechLevel, extraFilter);
     }
 
-    chooseUnit(
+    choicesHeroes(space: Card[], leastTechLevel?: TechLevel, mostTechLevel?: TechLevel, extraFilter?: (card: Card) => boolean): Card[] {
+        return this.choicesCharacters(space, false, true, leastTechLevel, mostTechLevel, extraFilter);
+    }
+
+    choicesUnitsOrHeroes(
         space: Card[],
-        chooseNumber: number,
-        choicesRequired: boolean,
-        usesTargetingRules: boolean,
         leastTechLevel?: TechLevel,
         mostTechLevel?: TechLevel,
         extraFilter?: (card: Card) => boolean
-    ) {
-        this.chooseCharacter(
-            space,
-            true,
-            false,
-            chooseNumber,
-            choicesRequired,
-            usesTargetingRules,
-            leastTechLevel,
-            mostTechLevel,
-            extraFilter
-        );
+    ): Card[] {
+        return this.choicesCharacters(space, true, true, leastTechLevel, mostTechLevel, extraFilter);
     }
 
-    chooseHero(
-        space: Card[],
-        chooseNumber: number,
-        choicesRequired: boolean,
-        usesTargetingRules: boolean,
-        leastTechLevel?: TechLevel,
-        mostTechLevel?: TechLevel,
-        extraFilter?: (card: Card) => boolean
-    ) {
-        this.chooseCharacter(
-            space,
-            false,
-            true,
-            chooseNumber,
-            choicesRequired,
-            usesTargetingRules,
-            leastTechLevel,
-            mostTechLevel,
-            extraFilter
-        );
-    }
-
-    chooseUnitOrHero(
-        space: Card[],
-        chooseNumber: number,
-        choicesRequired: boolean,
-        usesTargetingRules: boolean,
-        leastTechLevel?: TechLevel,
-        mostTechLevel?: TechLevel,
-        extraFilter?: (card: Card) => boolean
-    ) {
-        this.chooseCharacter(
-            space,
-            true,
-            true,
-            chooseNumber,
-            choicesRequired,
-            usesTargetingRules,
-            leastTechLevel,
-            mostTechLevel,
-            extraFilter
-        );
-    }
-
-    chooseCharacter(
+    choicesCharacters(
         space: Card[],
         includeUnits = true,
         includeHeroes = true,
-        chooseNumber: number,
-        choicesRequired: boolean,
-        usesTargetingRules: boolean,
         leastTechLevel?: TechLevel,
         mostTechLevel?: TechLevel,
         extraFilter?: (card: Card) => boolean
-    ) {
-        this.choose(
-            undefined,
-            space.filter(card => {
-                if (leastTechLevel !== undefined && leastTechLevel > card.techLevel) return false;
-                else if (mostTechLevel !== undefined && mostTechLevel < card.techLevel) return false;
-                else if (extraFilter && !extraFilter(card)) return false;
-                else if (includeUnits && card.cardType == 'Unit') return true;
-                else if (includeHeroes && card.cardType == 'Hero') return true;
-                else return false;
-            }),
-            chooseNumber,
-            choicesRequired,
-            usesTargetingRules
-        );
+    ): Card[] {
+        return space.filter(card => {
+            if (leastTechLevel !== undefined && leastTechLevel > card.techLevel) return false;
+            else if (mostTechLevel !== undefined && mostTechLevel < card.techLevel) return false;
+            else if (extraFilter && !extraFilter(card)) return false;
+            else if (includeUnits && card.cardType == 'Unit') return true;
+            else if (includeHeroes && card.cardType == 'Hero') return true;
+            else return false;
+        });
     }
 
     /**
      * Note this may return more than numberWeakest targets if there's a tie, and the user will have to choose.
      * Normally this isn't a targeting thing; it automatically happens to the weakest things.
      */
-    chooseWeakest(space: Card[], numberWeakest: number, usesTargetingRules: boolean, useAttack = true) {
+    choicesWeakest(space: Card[], numberWeakest: number, usesTargetingRules: boolean, useAttack = true): Card[] {
         let filtered = space.filter(card => card.cardType != 'Unit');
 
         // pick weakest tech level, using attack as a tie-breaker (if applicable). ties are still possible though
@@ -189,7 +120,7 @@ export abstract class Ability {
             }
         }
 
-        this.choose(undefined, selectedCards, numberWeakest, usesTargetingRules);
+        return selectedCards;
     }
 
     abstract resolveChoice(): EventDescriptor;
